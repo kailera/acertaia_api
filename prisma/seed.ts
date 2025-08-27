c5e5a032c4b230a4a1d6090d24ed5742702dee7a
Kailera <56659030+kailera@users.noreply.github.com>
2025-08-27 15:36:36 -0300

    feat: add default campaign for leads

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,27 @@
+import { PrismaClient, Channel } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+        await prisma.campaign.upsert({
+                where: { id: "DEFAULT" },
+                update: {},
+                create: {
+                        id: "DEFAULT",
+                        name: "DEFAULT",
+                        channel: Channel.WEB,
+                        startDate: new Date(),
+                },
+        });
+}
+
+main()
+        .then(async () => {
+                await prisma.$disconnect();
+        })
+        .catch(async (e) => {
+                console.error(e);
+                await prisma.$disconnect();
+                process.exit(1);
+        });
 
