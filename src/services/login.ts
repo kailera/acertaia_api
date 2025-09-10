1444d2e6b83806467ea772fc8575714012230026
Kailera <56659030+kailera@users.noreply.github.com>
2025-09-10 18:19:45 -0300

    feat: implement JWT login

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,21 +1,30 @@
+import { SignJWT } from "jose";
 import { getUserbyEmail } from "../repositories/user-repository";
 import { verifyPassword } from "../utils/password";
 
-interface LoginResult{
-	success:boolean;
+interface LoginResult {
+	success: boolean;
 	user?: Awaited<ReturnType<typeof getUserbyEmail>>;
+	token?: string;
 }
 
-
-export const loginUser = async (email: string, password: string):Promise<LoginResult> => {
-	
+export const loginUser = async (
+	email: string,
+	password: string,
+): Promise<LoginResult> => {
 	const queryUser = await getUserbyEmail(email);
 	if (!queryUser) return { success: false };
 
 	const passwordMatch = await verifyPassword(password, queryUser.passwordHash);
 	if (!passwordMatch) {
-		return {success:false};
+		return { success: false };
 	}
-	// password passado
-	return {success:true, user:queryUser}
+
+	const secret = new TextEncoder().encode(process.env.JWT_SECRET || "");
+	const token = await new SignJWT({ userId: queryUser.id })
+		.setProtectedHeader({ alg: "HS256" })
+		.setExpirationTime("1h")
+		.sign(secret);
+
+	return { success: true, user: queryUser, token };
 };
